0849138814c6d727be8dc62478f4c079a18c56b7
yanoshi <googreimu@gmail.com>
2015-06-01 14:23:29 +0900

    Add for processing command line option.
    コマンドラインオプションパーサーを boost/program_options.hpp を使って記述

--- torch4microscopic/torch4microscopic.cpp
+++ torch4microscopic/torch4microscopic.cpp
@@ -3,9 +3,52 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <boost/program_options.hpp>
+
+
+using namespace std;
+using namespace boost::program_options;
 
 int main(int argc, char* argv[])
 {
+	//オプション定義
+	options_description inout_options("path setting");
+	options_description calc_options("calculation option");
+
+	inout_options.add_options()
+		("input,i", value<string>(), "input path")
+		("output,o", value<string>(), "output path");
+
+	calc_options.add_options()
+		("amp_exp", "enable amplifier (exponential growth method)")
+		("cutback", "enable subtraction for cutback (average lowest pixel)");
+
+	variables_map cmd_values;
+
+	try 
+	{
+		options_description option_marged("");
+		option_marged.add(inout_options).add(calc_options);
+
+		if (!cmd_values.count("input") || cmd_values.count("output"))
+		{
+			cout << option_marged << endl;
+		}
+
+		store(parse_command_line(argc, argv, option_marged), cmd_values);
+		notify(cmd_values);
+
+		
+	}
+	catch (exception &e)
+	{
+		cout << e.what() << endl;
+	}
+
+	
 	return 0;
 }
 
